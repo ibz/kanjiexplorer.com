84c8d3a9dca4e4916f9b76a170b658b760241309
Ionut Bizau <i@ibz.me>
2012-03-16 21:19:04 +0800

    JSON converter.

---
+++ to_json.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from collections import defaultdict
+import os
+import simplejson
+import sys
+import xml.parsers
+from xml.dom import minidom
+
+def load_elements():
+    elements = defaultdict(lambda: ([], []))
+    for e in os.listdir("xml/element"):
+        try:
+            x = minidom.parse("xml/element/%s" % e)
+        except xml.parsers.expat.ExpatError:
+            continue
+        element_id = int(x.getElementsByTagName('elementId')[0].childNodes[0].nodeValue)
+        subelement_ids = [int(se.childNodes[0].nodeValue) for se in x.getElementsByTagName('pElementId')[0].getElementsByTagName('int')]
+        subelement_ids = [s for s in subelement_ids if s != 0]
+        elements[element_id][0].extend(subelement_ids)
+        for subelement_id in subelement_ids:
+            elements[subelement_id][1].append(element_id)
+    return elements
+
+def main():
+    elements = load_elements()
+
+    print simplejson.dumps(elements)
+
+if __name__ == '__main__':
+    main()
