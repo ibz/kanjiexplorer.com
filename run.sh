5658a62f41e33c4e6e8b5a8d96d9c69578eb435d
Ionuț Bizău <i@ibz.me>
2020-11-21 15:44:29 +0800

    Auto stop.

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-docker run -d -e VIRTUAL_HOST=kanjiexplorer.com --network=webproxy --name=kanjiexplorer -e LETSENCRYPT_HOST=kanjiexplorer.com -e LETSENCRYPT_EMAIL=kanjiexplorer@ibz.me ibz0/kanjiexplorer.com
+docker stop kanjiexplorer.com
+docker container rm kanjiexplorer.com
+docker run -d -e VIRTUAL_HOST=kanjiexplorer.com --network=webproxy --name=kanjiexplorer.com -e LETSENCRYPT_HOST=kanjiexplorer.com -e LETSENCRYPT_EMAIL=kanjiexplorer@ibz.me ibz0/kanjiexplorer.com
